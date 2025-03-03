4017b1ffb22f72ebc649e85f79966af9cf6010e2
govind663 <whitdevil06@gmail.com>
2025-03-03 18:03:28 +0530

    Bhojwani-Constructions | V - 0.2

--- resources/views/components/backend/footer.blade.php
+++ resources/views/components/backend/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-md-12 footer-copyright text-center">
                 <p class="mb-0">
-                    Copyright {{ date('Y') }} J4C Group | All Rights Reserved | Designed & Developed by
+                    Copyright {{ date('Y') }} Bhojwani Construction | All Rights Reserved | Designed & Developed by
                     <a href="">Matrix Bricks</a>
                 </p>
             </div>
